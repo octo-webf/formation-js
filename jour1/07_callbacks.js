f1f6d2ab7fe05d6a3564b497a559eb883b3fd094
Pierre Trollé <ptrolle@octo.com>
2017-01-24 12:55:59 +0100

    ajout exercice callback

--- jour1/07_callbacks.js
+++ jour1/07_callbacks.js
@@ -1,1 +1,27 @@
-MAK help me;
+function test(callback) {
+  console.log('A : Fonction de test');
+  console.log('B : Je vais maintenant appeler la fonction de callback');
+
+  callback();
+}
+
+function la_fin() {
+  setTimeout(function () {
+    console.log('C : Je suis la fin');
+  }, 5)
+}
+
+function fin_alternative() {
+  console.log('D : Je suis la fin alternative');
+}
+
+function toto() {
+  console.log('E : Je suis la fonction toto');
+}
+
+console.log('F : J appelle ma function test qui appellera à la fin automatiquement ma callback');
+
+test(la_fin);
+toto();
+test(fin_alternative);
+
