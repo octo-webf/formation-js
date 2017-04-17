2113a6a2cbbb6fab28dea63721929d7893587fb7
Clément Dubois <cdubois@octo.com>
2017-04-17 22:33:10 +0200

    Redo prototype exercise

--- support-specificites-du-langage/04_prototypage.js
+++ support-specificites-du-langage/04_prototypage.js
@@ -1,3 +1,4 @@
+// Animal sera utilisé comme prototype de Horse
 var Animal = {
   age: 10,
 
@@ -6,23 +7,33 @@ var Animal = {
   }
 };
 
+// Horse est une simple fonction qui définit des attributs internes (constructor)
 function Horse() {
-
-  this.age = 20;
-
   this.talks = function () {
     console.log('Huuu Hu Hu Hu !');
   };
-  
-  return this;
 }
 
+//A partir de maintenant toutes les nouvelles instances de Horse auront Animal comme prototype
 Horse.prototype = Animal;
 
+// Un nouvel objet horse qui a comme prototype Animal
 var horse = new Horse();
-horse.printType();
+
+// Méthode liée au constructeur
 horse.talks();
+// Méthode trouvée en remontant la chaine de prototypage
+horse.printType();
+//L'age de horse définit par le constructeur
 console.log('horse.age = ' + horse.age);
 
+//Définition d'une "instance" d'animal grace à Object.create
+var animal = Object.create(Animal);
+console.log('animal.age = ', animal.age);
+
+//Redéfinition dynamique de l'age du prototype (dangereux)
 Horse.prototype.age = 30;
 console.log('horse.age = ' + horse.age);
+
+// ooops, on a redefinit 'age' pour tous les héritiers du prototype Animal
+console.log('animal.age = ', animal.age);
